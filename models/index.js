73943243ea4ff4997ce2e54cbdadade543c56ee7
Jill Cunningham <jleighcunningham@gmail.com>
2022-03-30 20:56:48 -0400

    updated index.js for models with appropriate associations

--- models/index.js
+++ models/index.js
@@ -1,31 +1,29 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id'
+  foreignKey: "id",
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-    foreignKey: 'id'
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_id',
-  foreignKey: 'id'
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tag_name',
-  foreignKey: 'id'
+  foreignKey: "tag_id",
 });
 
 module.exports = {
